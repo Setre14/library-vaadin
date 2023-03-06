94ae9312eb70991d1d8bbac376781d370365ed82
start.vaadin.com <no-reply@vaadin.com>
2023-03-06 13:03:09 +0000

    Generated project

---
+++ src/main/java/at/setre14/library/data/Role.java
@@ -0,0 +1,5 @@
+package at.setre14.library.data;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ src/main/java/at/setre14/library/data/service/UserRepository.java
@@ -0,0 +1,10 @@
+package at.setre14.library.data.service;
+
+import at.setre14.library.data.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
+    User findByUsername(String username);
+}
